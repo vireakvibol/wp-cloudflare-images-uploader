762b0fc36909d2605fd727aa8863a38eb435299e
VIREAK KEOSOKVIBOL <vireakvibol@gmail.com>
2022-04-09 16:41:14 +0700

    remove select box

--- wp-cloudflare-images-uploader.php
+++ wp-cloudflare-images-uploader.php
@@ -40,88 +40,82 @@ class WP_CLOUDFLARE_IMAGES_UPLOADER
 
   public function wp_handle_upload_prefilter($file)
   {
-    if (get_option('wp_cloudflare_images_uploader_enable'))
-    {
+    $ch = curl_init();
 
-      $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://api.cloudflare.com/client/v4/accounts/' . get_option('wp_cloudflare_images_uploader_account_id') .  '/images/v1');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    $args['file'] = new CURLFile($file['tmp_name'], $file['type'], $file['name']);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $args);
 
-      curl_setopt($ch, CURLOPT_URL, 'https://api.cloudflare.com/client/v4/accounts/' . get_option('wp_cloudflare_images_uploader_account_id') .  '/images/v1');
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-      curl_setopt($ch, CURLOPT_POST, 1);
-      $args['file'] = new CURLFile($file['tmp_name'], $file['type'], $file['name']);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, $args);
+    $headers = array();
+    $headers[] = 'Authorization: Bearer ' . get_option('wp_cloudflare_images_uploader_token');
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
-      $headers = array();
-      $headers[] = 'Authorization: Bearer ' . get_option('wp_cloudflare_images_uploader_token');
-      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    $result = curl_exec($ch);
 
-      $result = curl_exec($ch);
+    if (curl_errno($ch)) {
+        // echo 'Error:' . curl_error($ch);
+        return curl_error($ch);
+    }
+    curl_close($ch);
 
-      if (curl_errno($ch)) {
-          // echo 'Error:' . curl_error($ch);
-          return curl_error($ch);
-      }
-      curl_close($ch);
+    $result_decode = json_decode($result, true)['result'];
 
-      $result_decode = json_decode($result, true)['result'];
+    $attachment_id = wp_insert_attachment(array(
+      'guid' => $result_decode['variants'][0],
+      'post_mime_type' => $file['type'],
+      'post_title' => sanitize_title(pathinfo($file['name'])['filename']),
+    ));
 
-      $attachment_id = wp_insert_attachment(array(
-        'guid' => $result_decode['variants'][0],
-        'post_mime_type' => $file['type'],
-        'post_title' => sanitize_title(pathinfo($file['name'])['filename']),
-      ));
+    $attachment_metadata = array(
+      'width' => 200,
+      'height' => 400,
+      'file' => wp_basename($file['name']),
+      'sizes' => $file['size']
+    );
+    $attachment_metadata['sizes'] = array( 'full' => $attachment_metadata );
 
-      $attachment_metadata = array(
+    wp_update_attachment_metadata($attachment_id, $attachment_metadata);
+
+    // return wp_send_json(array(
+    //   'success' => true,
+    //   'id' => $attachment_id,
+    // ));
+    return wp_send_json(array(
+      'success' => true,
+      'data' => array(
+        'id' => $attachment_id,
+        'title' => sanitize_title(pathinfo($file['name'])['filename']),
+        'filename' => wp_basename($file['name']),
+        'url' => $result_decode['variants'][0],
+        'alt' => '',
+        'author' => get_current_user_id(),
+        'description' => '',
+        'caption' => '',
+        'name' => sanitize_title(pathinfo($file['name'])['filename']),
+        'status' => 'inherit',
+        'uploadedTo' => 0,
+        'date' => time() . 000,
+        'modified' => time() . 000,
+        'menuOrder' => 0,
+        'mime' => $file['type'],
+        'type' => 'image',
+        'subtype' => '',
+        'icon' => '',
+        'dateFormatted' => date('M d Y'),
+        'editLink' => get_admin_url() . 'post.php?post=' . $attachment_id . '&action=edit',
+        'meta' => false,
+        'authorName' => get_userdata(get_current_user_id())->display_name,
+        'authorLink' => get_author_posts_url(get_current_user_id()),
+        'filesizeInBytes' => $file['size'],
+        'filesizeHumanReadable' => size_format($file['size'], 2),
+        'context' => '',
         'width' => 200,
         'height' => 400,
-        'file' => wp_basename($file['name']),
-        'sizes' => $file['size']
-      );
-      $attachment_metadata['sizes'] = array( 'full' => $attachment_metadata );
-
-      wp_update_attachment_metadata($attachment_id, $attachment_metadata);
-
-      // return wp_send_json(array(
-      //   'success' => true,
-      //   'id' => $attachment_id,
-      // ));
-      return wp_send_json(array(
-        'success' => true,
-        'data' => array(
-          'id' => $attachment_id,
-          'title' => sanitize_title(pathinfo($file['name'])['filename']),
-          'filename' => wp_basename($file['name']),
-          'url' => $result_decode['variants'][0],
-          'alt' => '',
-          'author' => get_current_user_id(),
-          'description' => '',
-          'caption' => '',
-          'name' => sanitize_title(pathinfo($file['name'])['filename']),
-          'status' => 'inherit',
-          'uploadedTo' => 0,
-          'date' => time() . 000,
-          'modified' => time() . 000,
-          'menuOrder' => 0,
-          'mime' => $file['type'],
-          'type' => 'image',
-          'subtype' => '',
-          'icon' => '',
-          'dateFormatted' => date('M d Y'),
-          'editLink' => get_admin_url() . 'post.php?post=' . $attachment_id . '&action=edit',
-          'meta' => false,
-          'authorName' => get_userdata(get_current_user_id())->display_name,
-          'authorLink' => get_author_posts_url(get_current_user_id()),
-          'filesizeInBytes' => $file['size'],
-          'filesizeHumanReadable' => size_format($file['size'], 2),
-          'context' => '',
-          'width' => 200,
-          'height' => 400,
-          'orientation' => 'landscape',
-        )
-      ));
-    }
-
-    return $file;
+        'orientation' => 'landscape',
+      )
+    ));
   }
 
   public static function init()
@@ -131,10 +125,6 @@ class WP_CLOUDFLARE_IMAGES_UPLOADER
 
   public function admin_init()
   {
-
-    register_setting('media', 'wp_cloudflare_images_uploader_enable', array(
-      'type' => 'boolean'
-    ));
     register_setting('media', 'wp_cloudflare_images_uploader_account_id', array(
       'type' => 'string'
     ));
@@ -145,19 +135,9 @@ class WP_CLOUDFLARE_IMAGES_UPLOADER
     add_settings_section(
       'wp_cloudflare_images_uploader_setting_section',
       'Connect to Cloudflare Images',
-      'wp_cloudflare_images_uploader_setting_section_callback_function',
+      '',
       'media'
     );
-    function wp_cloudflare_images_uploader_setting_section_callback_function( $arg )
-    {
-      $option = get_option('wp_cloudflare_images_uploader_enable', '');
-      ?>
-
-      <label for="<?php $arg['id'] ?>'_enable">
-      <input id="<?php $arg['id'] ?>'_enable" name="wp_cloudflare_images_uploader_enable" type="checkbox" value="1" <?php checked($option); ?> /> Enable Cloudflare Images Uploader
-      </label>
-      <?php
-    }
 
     add_settings_field(
       'wp_cloudflare_images_uploader_setting_field_account_id',
